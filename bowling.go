b13264eb3af35cbfbf490a2b6c4488432431a388
Michael Whatcott <mdwhatcott@gmail.com>
2019-10-22 21:58:16 -0600

    Third test: a spare.

--- bowling.go
+++ bowling.go
@@ -1,13 +1,46 @@
 package bowling
 
 type Game struct {
-	score int
+	score  int
+	throw  int
+	throws [21]int
 }
 
-func (this *Game) Roll(pins int) {
-	this.score += pins
+func (this *Game) RecordRoll(pins int) {
+	this.throws[this.throw] = pins
+	this.throw++
 }
 
-func (this *Game) Score() int {
+func (this *Game) CalculateScore() int {
+	this.throw = 0
+	for frame := 0; frame < 10; frame++ {
+		if this.currentFrameIsSpare() {
+			this.scoreSpareFrame()
+		} else {
+			this.scoreRegularFrame()
+		}
+	}
 	return this.score
+}
+
+func (this *Game) currentFrameIsSpare() bool {
+	return this.frameScore() == 10
+}
+
+func (this *Game) scoreSpareFrame() {
+	this.score += 10 + this.pins(2)
+	this.throw += 2
+}
+
+func (this *Game) scoreRegularFrame() {
+	this.score += this.frameScore()
+	this.throw += 2
+}
+
+func (this *Game) frameScore() int {
+	return this.pins(0) + this.pins(1)
+}
+
+func (this *Game) pins(offset int) int {
+	return this.throws[this.throw+offset]
 }
--- bowling_test.go
+++ bowling_test.go
@@ -19,11 +19,16 @@ func (this *GameFixture) Setup() {
 	this.game = new(Game)
 }
 func (this *GameFixture) assertScore(expected int) bool {
-	return this.AssertEqual(expected, this.game.Score())
+	return this.AssertEqual(expected, this.game.CalculateScore())
 }
 func (this *GameFixture) rollMany(times, pins int) {
 	for x := 0; x < times; x++ {
-		this.game.Roll(pins)
+		this.game.RecordRoll(pins)
+	}
+}
+func (this *GameFixture) rollSeveral(throws ...int) {
+	for _, throw := range throws {
+		this.game.RecordRoll(throw)
 	}
 }
 func (this *GameFixture) TestGutterGame() {
@@ -33,4 +38,8 @@ func (this *GameFixture) TestGutterGame() {
 func (this *GameFixture) TestAllOnes() {
 	this.rollMany(20, 1)
 	this.assertScore(20)
+}
+func (this *GameFixture) TestSpare() {
+	this.rollSeveral(5, 5, 3)
+	this.assertScore(16)
 }
